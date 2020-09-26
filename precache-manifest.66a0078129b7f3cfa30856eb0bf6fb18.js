29834673b4d9b0bb3ed854a4fbb092ba7e17b28b
maulanamaleek <67380302+maulanamaleek@users.noreply.github.com>
2020-09-26 13:23:07 +0700

    Updates

---
+++ precache-manifest.66a0078129b7f3cfa30856eb0bf6fb18.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "002306709303099c4283b328a9348eb6",
+    "url": "/quickshop/index.html"
+  },
+  {
+    "revision": "45515fcda1221d331355",
+    "url": "/quickshop/static/css/main.cb45274d.chunk.css"
+  },
+  {
+    "revision": "bbe0e89ace1d0a32c8a3",
+    "url": "/quickshop/static/js/2.e5012747.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quickshop/static/js/2.e5012747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45515fcda1221d331355",
+    "url": "/quickshop/static/js/main.5e3a1c17.chunk.js"
+  },
+  {
+    "revision": "d69e7cc0b7b0ce6c53f4",
+    "url": "/quickshop/static/js/runtime-main.cb5af680.js"
+  },
+  {
+    "revision": "056f526e23187e5834d7a720ef5a7497",
+    "url": "/quickshop/static/media/alex.056f526e.jpg"
+  },
+  {
+    "revision": "ed093238e9f07fe6185a24434852e319",
+    "url": "/quickshop/static/media/bag.ed093238.jpg"
+  },
+  {
+    "revision": "fcea540d2af395275c9680b4a1e0bec3",
+    "url": "/quickshop/static/media/basket.fcea540d.jpg"
+  },
+  {
+    "revision": "c2e471b2e0f560c94a5a7d10dc97ce01",
+    "url": "/quickshop/static/media/canon.c2e471b2.jpg"
+  },
+  {
+    "revision": "334802d3157964d7b0372c0a1f139d69",
+    "url": "/quickshop/static/media/cat.334802d3.jpg"
+  },
+  {
+    "revision": "8ec58fc1e48b47bf45fbc9ddd49c56d6",
+    "url": "/quickshop/static/media/fan.8ec58fc1.jpg"
+  },
+  {
+    "revision": "1ed136e3a539cf8520eab881cd425e97",
+    "url": "/quickshop/static/media/hawai.1ed136e3.png"
+  },
+  {
+    "revision": "52a7337fc47eb32f6d1c273f83f00a60",
+    "url": "/quickshop/static/media/jeans.52a7337f.png"
+  },
+  {
+    "revision": "ec92a79ec0ab7af1819789259f36af2e",
+    "url": "/quickshop/static/media/jogging.ec92a79e.png"
+  },
+  {
+    "revision": "b461b2f1efbf3c2ebe4a6098506c15c6",
+    "url": "/quickshop/static/media/jordan.b461b2f1.jpg"
+  },
+  {
+    "revision": "a0655ae4b9a2d070a724d224a49282e0",
+    "url": "/quickshop/static/media/kaos.a0655ae4.jpg"
+  },
+  {
+    "revision": "224e95ebeb65c7fdc96e63fc920aa940",
+    "url": "/quickshop/static/media/kemeja.224e95eb.jpg"
+  },
+  {
+    "revision": "5c938b95d25b94a33ec417be8fd2de5a",
+    "url": "/quickshop/static/media/key.5c938b95.jpg"
+  },
+  {
+    "revision": "c39fcf1b0ac6bdf9a48e1825a9129db3",
+    "url": "/quickshop/static/media/kid.c39fcf1b.jpg"
+  },
+  {
+    "revision": "397ae88732dd308adc3116f71f5ad1ae",
+    "url": "/quickshop/static/media/macbook.397ae887.jpg"
+  },
+  {
+    "revision": "ed62ee47727170d4257346d5d504031c",
+    "url": "/quickshop/static/media/necklace.ed62ee47.jpg"
+  },
+  {
+    "revision": "d42f8ef14cf66ca4421553a05de6b455",
+    "url": "/quickshop/static/media/oppo.d42f8ef1.jpg"
+  },
+  {
+    "revision": "2496fb1b236a1ccd4fc891060f6812fd",
+    "url": "/quickshop/static/media/pensil.2496fb1b.jpg"
+  },
+  {
+    "revision": "b3a3ef432597633288204babe5ab93b1",
+    "url": "/quickshop/static/media/skateboard.b3a3ef43.jpg"
+  },
+  {
+    "revision": "74b9d65d8f9045d8528b18231c1e19e5",
+    "url": "/quickshop/static/media/water.74b9d65d.jpg"
+  },
+  {
+    "revision": "b37245cd9aad21cba18f61c97cebfd20",
+    "url": "/quickshop/static/media/whiskas.b37245cd.jpg"
+  }
+]);
